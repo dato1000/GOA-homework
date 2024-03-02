842792858e1e025855f10f8576d981de7ca283fb
dato1000 <prodato2009@gmail.com>
2024-03-02 10:54:57 +0300

    first commit

---
+++ day 03/lessone3.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter the first number"))
+num2 = int(input("enter the first number"))
+print(num1 + num2)
+
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+travel = input("if you could, how many years from now would you travel: ")
+
+print(name, age, travel)
